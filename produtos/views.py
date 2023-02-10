dae12907237b7b6a4e4d54f48d3b270ba8e479fd
Lorran <lorran0202fps@gmail.com>
2023-02-10 19:48:34 -0300

    adiciona post dos dados na view

--- produtos/views.py
+++ produtos/views.py
@@ -1,21 +1,36 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from produtos.models import Produto
+from produtos.forms import ProdutoModelForm
 
 def listagem_produtos(request):
+    produtos = Produto.objects.all() # <----- Utiliza o ORM do Django
     produtos_dos_vendedores = [{
-
-        'vendedor': {'nome': 'Lorran'},
-        'produtos': [
-            {'nome': 'Uva', 'preco': 1}, 
-            {'nome': 'Melancia', 'preco': 2},
-            {'nome': 'Banana', 'preco': 3},
-            ]
-    },
-    {
-        'vendedor': {'nome': 'Jefferson'},
-        'produtos': [
-            {'nome': 'Goiaba', 'preco': 3}, 
-            
-            ]
+        'vendedor': {'nome': 'John Doe'},
+        'produtos': produtos
     }]
-    context = {'produtos_dos_vendedores': produtos_dos_vendedores}
-    return render(request, 'templates/listagem_produtos.html',context)
+    context = {'produtos_dos_vendedores': produtos_dos_vendedores }
+    return render(request, 'templates/listagem_produtos.html', context)
+
+def detalhamento_produto(request, id):
+    produto = get_object_or_404(Produto, pk=id)
+    context = {
+        'produto': produto
+    }
+    return render(request, 'templates/detalhamento_produto.html', context)
+
+
+def cadastro_produto(request):
+    form = ProdutoModelForm()
+    context = {
+        'form': form
+    }
+    if request.method == 'GET':
+        print('É um GET')
+    if request.method == 'POST':
+        print('É um POST')
+        form = ProdutoModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/produtos/')
+    return render(request, 'templates/cadastrar_produto.html', context)
